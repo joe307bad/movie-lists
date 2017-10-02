35babfded442e18d53318253ba3291c9f7db1cbc
Joe <badaczewskij@live.com>
2017-10-02 10:34:36 -0400

    Initial Commit

---
+++ MovieLists.DB/Context.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CMMI_CC.DB
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options) { }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>().ToTable("Restaurants");
+            modelBuilder.Entity<Review>().ToTable("Reviews");
+            modelBuilder.Entity<User>().ToTable("Users");
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=CMMICC;Integrated Security=True;MultipleActiveResultSets=True;");
+        }
+
+        public override int SaveChanges()
+        {
+            AddTimestamps();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            AddTimestamps();
+            return await base.SaveChangesAsync();
+        }
+
+        private void AddTimestamps()
+        {
+            var entities = ChangeTracker
+                .Entries()
+                .Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+            
+            foreach (var entity in entities)
+            {
+                if (entity.State == EntityState.Added)
+                {
+                    ((BaseEntity)entity.Entity).Created = DateTime.UtcNow;
+                }
+
+                ((BaseEntity)entity.Entity).Modified = DateTime.UtcNow;
+            }
+        }
+
+    }
+
+}
---
+++ MovieLists.DB/ContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CMMI_CC.DB
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<Context>
+    {
+        public Context CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<Context>();
+            builder.UseSqlServer(
+                "Server=./SQLEXPRESS;Database=CMMICC;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new Context(builder.Options);
+        }
+    }
+}
